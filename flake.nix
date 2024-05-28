1438012d7b7bed44f0c72f643397bc133c04ff5f
sajenim <its.jassy@pm.me>
2024-05-28 09:00:10 +0800

    Add nix development environment

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "Sajenim's Minecraft Modpack";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [ packwiz unzip zip yq ];
+        };
+      });
+}
